13bbb17052fdd6be71fe7feac1c4dcdc55e83c32
kimJaeHyeon <kmr2644@gmail.com>
2022-04-27 21:58:11 +0900

    Initial commit

---
+++ src/main/java/capstone/server/BucketApplication.java
@@ -0,0 +1,13 @@
+package capstone.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BucketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BucketApplication.class, args);
+	}
+
+}
