05723ccbe097a31706a64c713b802a948dd7645b
kimJaeHyeon <kmr2644@gmail.com>
2022-06-01 23:46:19 +0900

    스토리지 서비스, redis 고민

--- src/main/java/capstone/server/config/FileUploadProperties.java
+++ src/main/java/capstone/server/config/FileUploadProperties.java
@@ -6,4 +6,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class FileUploadProperties {
     private String uploadDir;
 
+    public String getUploadDir() {
+        return uploadDir;
+    }
+
+    public void setUploadDir(String uploadDir) {
+        this.uploadDir = uploadDir;
+    }
 }
--- src/main/java/capstone/server/service/ImageStorageService.java
+++ src/main/java/capstone/server/service/ImageStorageService.java
@@ -1,11 +1,18 @@
 package capstone.server.service;
 
 
+import capstone.server.config.FileUploadProperties;
 import capstone.server.repository.ProfileImageRepository;
 import capstone.server.repository.bucket.BucketImageRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Service
 @RequiredArgsConstructor
@@ -14,10 +21,22 @@ public class ImageStorageService {
     private final BucketImageRepository bucketImageRepository;
     private final ProfileImageRepository profileImageRepository;
 
+    private final Path dirLocation;
+
+    @Autowired
+    public ImageStorageService(BucketImageRepository bucketImageRepository, ProfileImageRepository profileImageRepository,FileUploadProperties fileUploadProperties) {
+        this.bucketImageRepository = bucketImageRepository;
+        this.profileImageRepository = profileImageRepository;
+        this.dirLocation = Paths.get(fileUploadProperties.getUploadDir())
+                                .toAbsolutePath()
+                                .normalize();
+    }
 
     @Transactional
-    public String profileImageSave() {
+    public String profileImageSave(MultipartFile file) {
 
+        String fileName = file.getName();
+        Path location = this.dirLocation.resolve(fileName);
 
     }
 
