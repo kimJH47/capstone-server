d41f87debca050648b433b3659c6ce92dbb6e710
kimJaeHyeon <kmr2644@gmail.com>
2022-06-02 20:47:27 +0900

    aws s3 설정

---
+++ src/main/java/capstone/server/commons/S3Uploader.java
@@ -0,0 +1,27 @@
+package capstone.server.commons;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Component
+public class S3Uploader {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    public String bucket;
+
+    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+        File uploadFile = convert(multipartFile)  // 파일 변환할 수 없으면 에러
+                                                  .orElseThrow(() -> new IllegalArgumentException("error: MultipartFile -> File convert fail"));
+
+        return upload(uploadFile, dirName);
+    }
+
+}
